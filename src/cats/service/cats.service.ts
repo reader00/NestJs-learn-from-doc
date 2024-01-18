72b49265b95445be0125cd9077aeeab965a1fee0
reader00 <qoryamanahputra0708@gmail.com>
2024-01-18 21:13:54 +0700

    implement service

--- src/cats/service/cats.service.ts
+++ src/cats/service/cats.service.ts
@@ -1,27 +1,48 @@
 import { Injectable } from '@nestjs/common';
-import { CatRepository } from '../domain';
+import { CatRepository, CreateCatDto, UpdateCatDto } from '../domain';
 import { Cat } from '../domain/interfaces';
 
 @Injectable()
 export class CatsService extends CatRepository {
+    private readonly cats: Cat[] = []
+
+    private _lastId(): number {
+        return this.cats.length ? this.cats[this.cats.length - 1].id : 0
+    }
+    private _getIndex(id: number): number {
+        return this.cats.findIndex(cat => cat.id === id)
+    }
 
     getCats(): Cat[] {
-        return
+        return this.cats;
     }
 
-    getCatById(): Cat {
-        return
+    getCatById(id: number): Cat {
+        const found = this.cats.filter(cat => cat.id === id)
+        return found[0]
     }
 
-    createCat(): Cat {
-        return
+    createCat(cat: CreateCatDto): Cat {
+        const id = this._lastId() + 1
+        const newCat = { ...cat, createdAt: +new Date(), id }
+        this.cats.push(newCat)
+        return newCat
     }
 
-    updateCatById(): Cat {
-        return
+    updateCatById(id: number, cat: UpdateCatDto): Cat {
+        const index = this._getIndex(id)
+        if (index === -1) return
+        const oldCat = this.cats[index]
+        const updated = { ...oldCat, ...cat, id }
+        oldCat[index] = updated
+        return updated
     }
 
-    deleteCatById(): Cat {
-        return
+    deleteCatById(id: number): Cat {
+        const index = this._getIndex(id)
+        if (index === -1) return
+        const deletedCat = this.cats[index]
+        this.cats.splice(index, 1)
+        return deletedCat
     }
 }
