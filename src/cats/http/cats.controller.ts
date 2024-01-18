f06f78b951fda5f027638bdff25b94e53751fc4c
reader00 <qoryamanahputra0708@gmail.com>
2024-01-18 21:26:32 +0700

    change directory structure

--- src/cats/http/cats.controller.ts
+++ src/cats/http/cats.controller.ts
@@ -1,31 +1,34 @@
 import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
-import { CatQuery, CreateCatDto, UpdateCatDto } from './domain';
+import { CatQuery, CreateCatDto, UpdateCatDto } from '../domain';
+import { CatsService } from '../service/cats.service';
 
 @Controller('cats')
 export class CatsController {
 
+    constructor(private catService: CatsService) { }
+
     @Post()
     create(@Body() createCatDto: CreateCatDto) {
-        return "New cat added"
+        return this.catService.createCat(createCatDto)
     }
 
     @Get()
     findAll(@Query() query: CatQuery) {
-        return `All the cats returned${query.limit ? ' (limit: ' + query.limit + ')' : ''}.`
+        return this.catService.getCats()
     }
 
     @Get(':id')
     findOne(@Param('id', ParseIntPipe) id) {
-        return `Cat with id ${id} returned`
+        return this.catService.getCatById(id)
     }
 
     @Patch(':id')
     update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateCatDto) {
-        return 'Cat updated'
+        return this.catService.updateCatById(id, body)
     }
 
     @Delete(":id")
     delete(@Param('id', ParseIntPipe) id: number) {
-        return 'Cats deleted'
+        return this.catService.deleteCatById(id)
     }
 }
