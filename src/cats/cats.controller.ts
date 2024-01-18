5b497c2d8346f4d0e0d42209d66a110a651deb70
reader00 <qoryamanahputra0708@gmail.com>
2024-01-18 18:14:22 +0700

    create basic CRUD controller

--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,31 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, ParseIntPipe, Patch, Post, Query } from '@nestjs/common';
+import { CatQuery, CreateCatDto, UpdateCatDto } from './domain';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+
+    @Post()
+    create(@Body() createCatDto: CreateCatDto) {
+        return "New cat added"
+    }
+
+    @Get()
+    findAll(@Query() query: CatQuery) {
+        return `All the cats returned${query.limit ? ' (limit: ' + query.limit + ')' : ''}.`
+    }
+
+    @Get(':id')
+    findOne(@Param('id', ParseIntPipe) id) {
+        return `Cat with id ${id} returned`
+    }
+
+    @Patch(':id')
+    update(@Param('id', ParseIntPipe) id: number, @Body() body: UpdateCatDto) {
+        return 'Cat updated'
+    }
+
+    @Delete(":id")
+    delete(@Param('id', ParseIntPipe) id: number) {
+        return 'Cats deleted'
+    }
+}
