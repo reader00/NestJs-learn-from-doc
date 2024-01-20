c950414cf3a345129908f46baebee281b5501cf7
reader00 <qoryamanahputra0708@gmail.com>
2024-01-21 01:38:49 +0700

    apply payload

--- src/common/decorators/payload.decorator.ts
+++ src/common/decorators/payload.decorator.ts
@@ -3,7 +3,7 @@ import { ExecutionContext, createParamDecorator } from "@nestjs/common";
 export const Payload = createParamDecorator(
     (data: unknown, ctx: ExecutionContext) => {
         const request = ctx.switchToHttp().getRequest()
-        console.log(request);
+        console.log(request.body);
         return "Payload decorator"
     }
 )
