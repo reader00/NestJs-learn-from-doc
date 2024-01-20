abe402d785ed3f152e3c08667a6bb65f37ed01fc
reader00 <qoryamanahputra0708@gmail.com>
2024-01-20 22:16:42 +0700

    apply roles guard

--- src/cats/http/cats.controller.ts
+++ src/cats/http/cats.controller.ts
@@ -1,8 +1,10 @@
-import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Patch, Post, Query, UseFilters, UsePipes, ValidationPipe } from '@nestjs/common';
+import { Body, Controller, Delete, Get, HttpStatus, Param, ParseIntPipe, Patch, Post, Query, UseFilters, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
 import { CatQuery, CatRepository, CreateCatDto, UpdateCatDto } from '../domain';
 import { HttpExceptionFilter } from '../../common/exceptions/exceptions';
 import { ZodValidationPipe } from '../../common/pipes';
-import { createCatSchema } from '../../common/schema';
+import { createCatSchema } from '../../common/schemas';
+import { Roles } from '../../common/decorators/roles.decorator';
+import { RolesGuard } from '../service/roles.guard';
 
 // @UseFilters(HttpExceptionFilter)
 @Controller('cats')
@@ -11,6 +13,8 @@ export class CatsController {
     constructor(private catService: CatRepository) { }
 
     @Post()
+    @Roles(['admin'])
+    @UseGuards(RolesGuard)
     // @UsePipes(new ZodValidationPipe(createCatSchema))
     create(@Body(ValidationPipe) createCatDto: CreateCatDto) {
         return this.catService.createCat(createCatDto)
