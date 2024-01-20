9efe2887df96e682c703fce37e1a674580564238
reader00 <qoryamanahputra0708@gmail.com>
2024-01-20 18:16:13 +0700

    add validation in dto:(using 3rd-party package, class-validator)

--- src/cats/domain/dto.ts
+++ src/cats/domain/dto.ts
@@ -1,10 +1,18 @@
+import { IsNumber, IsOptional, IsString } from "class-validator"
+
 export class CreateCatDto {
+    @IsString()
     name: string
+
+    @IsNumber()
     age: number
+
+    @IsString()
     breed: string
 }
 
 export class CatQuery {
+    @IsOptional()
     limit?: number
 }
 
