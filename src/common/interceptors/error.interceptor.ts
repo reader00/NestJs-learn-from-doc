0f62bbb5f16e3e57a74f363176c5bca072c4f4d7
reader00 <qoryamanahputra0708@gmail.com>
2024-01-20 23:50:26 +0700

    add error interceptor

---
+++ src/common/interceptors/error.interceptor.ts
@@ -0,0 +1,18 @@
+import { BadGatewayException, CallHandler, ExecutionContext, Injectable, NestInterceptor } from "@nestjs/common";
+import { Observable, catchError, pipe, throwError } from "rxjs";
+
+@Injectable()
+export class ErrorInterceptor implements NestInterceptor {
+
+    intercept(context: ExecutionContext, next: CallHandler<any>): Observable<any> | Promise<Observable<any>> {
+
+        return next.
+            handle().
+            pipe(
+                catchError(err => throwError(() => new BadGatewayException())
+                )
+            )
+
+    }
+
+}
