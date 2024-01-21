7520d5960e8154a5bc59ffb347a36a96ae09c0d1
reader00 <qoryamanahputra0708@gmail.com>
2024-01-21 09:51:21 +0700

    remove config module from app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,7 +5,7 @@ import { CatsModule } from './cats/cats.module';
 import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [CatsModule, ConfigModule],
+  imports: [CatsModule],
   controllers: [AppController],
   providers: [AppService],
 })
