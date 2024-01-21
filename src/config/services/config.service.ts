f60ab143008c61a51e706ebca25409d719d828c7
reader00 <qoryamanahputra0708@gmail.com>
2024-01-21 09:51:07 +0700

    apply options from configrable module

--- src/config/services/config.service.ts
+++ src/config/services/config.service.ts
@@ -4,13 +4,15 @@ import path from 'path';
 import dotenv from 'dotenv'
 import fs from 'fs'
 import { CONFIG_OPTIONS } from '../../common/constants';
+import { MODULE_OPTIONS_TOKEN } from '../config.module-definition';
+import { ConfigModuleOptions } from '../domain';
 
 @Injectable()
 export class ConfigService {
 
     private readonly envConfig: EnvConfig
 
-    constructor(@Inject(CONFIG_OPTIONS) private options: Record<string, any>) {
+    constructor(@Inject(MODULE_OPTIONS_TOKEN) private options: ConfigModuleOptions) {
         const filePath = `${process.env.NODE_ENV || 'development'}.env`
         const envFile = path.resolve(__dirname, '../../', options.folder, filePath)
         this.envConfig = dotenv.parse(fs.readFileSync(envFile))
